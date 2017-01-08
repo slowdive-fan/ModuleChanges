93baaf1e7aa7b8a66072e74583e8676ab05c4737
slowdive-fan <slowdive.fam@gmail.com>
2017-01-08 14:16:28 -0700

    initial module
    first version of The Elderin Stone

---
+++ The Elderin Stone/scripts/gcCheckJournalEntryByTag.cs
@@ -0,0 +1,5 @@
+//gcCheckJournalEntryByTag.cs - Checks to see if an entry to the player's journal is at a specific EntryId point
+//parm1 = (string) module's categoryTag that the entry belongs to
+//parm2 = (string) compare operator ( = or > or < or ! )
+//parm3 = (int) EntryId of the journal entry
+//parm4 = none
